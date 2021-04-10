e8e4100a609be8a247b8204eda3310683ece0ab8
smonajemi <sina.monajemi@me.com>
2021-04-10 19:58:38 -0400

    commit

--- dist/js/loader.js
+++ dist/js/loader.js
@@ -1,12 +1,10 @@
-(function($){
-    'use strict';
-      $(window).on('load', function () {
-        setTimeout(function(){
-            $('.preloader').fadeOut('slow');
-            $('.loader').fadeOut('slow');
-        },1000);
-          
-      });
-  })(jQuery)
-  
-  document.getElementsByClassName("animsition-loading").style.visibility = "hidden";
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
