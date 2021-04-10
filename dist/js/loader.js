785376d53d0c38282562b566b17afab8af56a696
smonajemi <sina.monajemi@me.com>
2021-04-10 18:24:06 -0400

    commit

---
+++ dist/js/loader.js
@@ -0,0 +1,12 @@
+(function($){
+    'use strict';
+      $(window).on('load', function () {
+        setTimeout(function(){
+            $('.preloader').fadeOut('slow');
+            $('.ploader').fadeOut('slow');
+        },1000);
+          
+      });
+  })(jQuery)
+  
+  
