ce7bcfeedfd0f0fbac3e909ed47dc08b0d208fc5
smonajemi <sina.monajemi@me.com>
2021-04-11 02:04:03 -0400

    commit

--- dist/js/loader.js
+++ dist/js/loader.js
@@ -1,9 +1,11 @@
 var myVar;
 
 function myFunction() {
-  myVar = setTimeout(showPage, 3000);
+  myVar = setTimeout(showPage, 1000);
 }
 
 function showPage() {
+
   document.getElementById("preloader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
 }
