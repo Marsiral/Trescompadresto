a2e527233a6e784569bdf7eb4f1015e7fe9ad103
smonajemi <sina.monajemi@me.com>
2021-04-10 20:48:47 -0400

    commit

--- dist/js/loader.js
+++ dist/js/loader.js
@@ -6,5 +6,5 @@ function myFunction() {
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+  document.getElementById("loadtherest").style.display = "block";
 }
