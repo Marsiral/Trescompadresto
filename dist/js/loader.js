9c88be89821491589184d829870597e326de9aeb
smonajemi <sina.monajemi@me.com>
2021-04-10 22:28:55 -0400

    commit

--- dist/js/loader.js
+++ dist/js/loader.js
@@ -1,11 +1,10 @@
-// var myVar;
+var myVar;
 
-// function myFunction() {
-//   myVar = setTimeout(showPage, 3000);
-// }
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
 
-// function showPage() {
-//   document.getElementById("loader").style.display = "none";
-//   document.getElementById("loadtherest").style.display = "block";
-// }
-
+function showPage() {
+  document.getElementById("preloader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
